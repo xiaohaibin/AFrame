68f0df82602f7f08c206b7f659ee8ec436a38e6d
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-10-11 17:20:17 +0800

    提交代码

---
+++ aframelibrary/src/main/java/com/stx/xiaohaibin/aframelibrary/base/BaseActivity.java
@@ -0,0 +1,171 @@
+package com.stx.xiaohaibin.aframelibrary.base;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: xiaohaibin.
+ * @time: 2018/9/21
+ * @mail:xhb_199409@163.com
+ * @github:https://github.com/xiaohaibin
+ * @describe: BaseActivity
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    private int REQUEST_CODE_PERMISSION = 0x00099;
+    //权限申请回调
+    private OnPermissionResponseListener onPermissionResponseListener;
+
+    /**
+     * 校验单个权限
+     * @param permission
+     * @return
+     */
+    protected boolean checkPermission(String permission) {
+        return ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * 请求权限
+     * <p>
+     * 警告：此处除了用户拒绝外，唯一可能出现无法获取权限或失败的情况是在AndroidManifest.xml中未声明权限信息
+     * Android6.0+即便需要动态请求权限（重点）但不代表着不需要在AndroidManifest.xml中进行声明。
+     * @param permissions                  请求的权限
+     * @param onPermissionResponseListener 回调监听器
+     */
+    public void requestPermission(OnPermissionResponseListener onPermissionResponseListener,String... permissions) {
+        this.onPermissionResponseListener = onPermissionResponseListener;
+        if (checkPermissions(permissions)) {
+            if (onPermissionResponseListener != null) {
+                onPermissionResponseListener.onSuccess(permissions);
+            }
+        } else {
+            List<String> needPermissions = getDeniedPermissions(permissions);
+            ActivityCompat.requestPermissions(this, needPermissions.toArray(new String[needPermissions.size()]), REQUEST_CODE_PERMISSION);
+        }
+    }
+
+    /**
+     * 检测所有的权限是否都已授权
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissions(String[] permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 获取权限集中需要申请权限的列表
+     * @param permissions
+     * @return
+     */
+    private List<String> getDeniedPermissions(String[] permissions) {
+        List<String> needRequestPermissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                needRequestPermissionList.add(permission);
+            }
+        }
+        return needRequestPermissionList;
+    }
+
+
+    /**
+     * 系统请求权限回调
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (verifyPermissions(grantResults)) {
+                if (onPermissionResponseListener != null) {
+                    onPermissionResponseListener.onSuccess(permissions);
+                }
+            } else {
+                if (onPermissionResponseListener != null) {
+                    onPermissionResponseListener.onFail();
+                }
+                showTipsDialog();
+            }
+        }
+    }
+
+    /**
+     * 确认所有的权限是否都已授权
+     * @param grantResults
+     * @return
+     */
+    private boolean verifyPermissions(int[] grantResults) {
+        for (int grantResult : grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 显示提示对话框
+     */
+    private void showTipsDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("警告")
+                .setMessage("需要必要的权限才可以正常使用该功能，您已拒绝获得该权限。 如果需要重新授权，您可以点击“允许”按钮进入系统设置进行授权")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startAppSettings();
+                    }
+                }).show();
+    }
+
+    /**
+     * 启动当前应用设置页面
+     */
+    public void startAppSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    public interface OnPermissionResponseListener {
+        void onSuccess(String[] permissions);
+
+        void onFail();
+    }
+
+}
