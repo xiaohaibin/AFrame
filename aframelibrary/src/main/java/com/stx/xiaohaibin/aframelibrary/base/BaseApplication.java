32dcc70df6d17dde3e43c0a2080d5397e9869107
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-10-12 15:08:02 +0800

    提交AFrame框架代码

---
+++ aframelibrary/src/main/java/com/stx/xiaohaibin/aframelibrary/base/BaseApplication.java
@@ -0,0 +1,21 @@
+package com.stx.xiaohaibin.aframelibrary.base;
+
+import android.app.Application;
+
+import com.blankj.utilcode.util.Utils;
+
+/**
+ * @author: xiaohaibin.
+ * @time: 2018/10/12
+ * @mail:xhb_199409@163.com
+ * @github:https://github.com/xiaohaibin
+ * @describe: BaseApplication
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+    }
+}
